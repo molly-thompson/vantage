4d713e405a8463b7490012f645cd5f1b06c8fc80
molly-thompson <mollyerinn@gmail.com>
2026-08-26 18:42:58 +0100

    chore: Configure root URL routing

--- vantage/urls.py
+++ vantage/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("logs/", include("logs.urls")),
     path("admin/", admin.site.urls),
 ]
