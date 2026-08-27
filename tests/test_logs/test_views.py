8289e083c162e102629eb73a9cc3d51c65f63cb1
molly-thompson <mollyerinn@gmail.com>
2026-08-27 15:01:10 +0100

    fix: Fix logs url test name

--- tests/test_logs/test_views.py
+++ tests/test_logs/test_views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 
 
-def test_accounts_index_view(client):
+def test_logs_index_view(client):
     response = client.get(reverse("logs:index"))
 
     assert response.status_code == 200
