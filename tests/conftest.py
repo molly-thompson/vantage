fd0accda81c34609858df83124b7a860c3411f1e
molly-thompson <mollyerinn@gmail.com>
2026-08-31 11:24:38 +0100

    test: Add User pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,18 @@ from typing import Any
 
 import pytest
 
+from accounts.models import User
+
 
 @pytest.fixture(autouse=True)
 def disable_ssl_redirect(settings: Any) -> None:
     settings.SECURE_SSL_REDIRECT = False
+
+
+@pytest.fixture
+def user() -> User:
+    return User.objects.create_user(
+        username="testuser",
+        email="user@example.com",
+        password="password123",
+    )
