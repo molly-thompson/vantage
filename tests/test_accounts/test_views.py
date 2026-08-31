23e383558816c286dc43812c85c3cb187df24f0f
molly-thompson <mollyerinn@gmail.com>
2026-08-31 11:55:22 +0100

    test: Add tests for Allauth URL configuration

--- tests/test_accounts/test_views.py
+++ tests/test_accounts/test_views.py
@@ -8,3 +8,22 @@ def test_accounts_dashboard_view(client: Client) -> None:
 
     assert response.status_code == 200
     assert response.content == b"Accounts dashboard loads"
+
+
+# TEST ALLAUTH URLS ARE CORRECTLY WIRED INTO PROJECT
+def test_allauth_login_url_is_wire(client: Client) -> None:
+    response = client.get(reverse("account_login"))
+
+    assert response.status_code == 200
+
+
+def test_allauth_signup_url_is_wire(client: Client) -> None:
+    response = client.get(reverse("account_signup"))
+
+    assert response.status_code == 200
+
+
+def test_allauth_logout_url_is_wire(client: Client) -> None:
+    response = client.get(reverse("account_logout"))
+
+    assert response.status_code == 302
