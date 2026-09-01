0a4b36152b4bc796b8817fa9726403672032a232
molly-thompson <mollyerinn@gmail.com>
2026-09-01 18:42:19 +0100

    test: Add pytest fixtures and test template for testing base.html

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
+from pathlib import Path
 from typing import Any
 
 import pytest
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.http import HttpRequest, HttpResponse
+from django.test import RequestFactory
 
 from accounts.models import User
 from logs.models import Log
@@ -45,3 +50,23 @@ def log(user: User) -> Log:
         body="Test log body",
         creator_user=user,
     )
+
+
+@pytest.fixture(autouse=True)
+def test_template_dir(settings: Any) -> None:
+    settings.TEMPLATES[0]["DIRS"].append(
+        Path(__file__).parent / "test_app_functionality"
+    )
+
+
+@pytest.fixture
+def message_request() -> HttpRequest:
+    request = RequestFactory().get("/")
+
+    def get_response(request: HttpRequest) -> HttpResponse:
+        return HttpResponse()
+
+    SessionMiddleware(get_response).process_request(request)
+    MessageMiddleware(get_response).process_request(request)
+
+    return request
