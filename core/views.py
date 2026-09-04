a31ec4f970b0b12cb5fa15a25bf0abe3b64cbd6a
molly-thompson <mollyerinn@gmail.com>
2026-09-04 14:54:50 +0100

    chore: Suppress mypy warning for login decorator

--- core/views.py
+++ core/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from global_login_required import login_not_required
 
 
-@login_not_required
+@login_not_required  # type: ignore[untyped-decorator]
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, "core/home.html")
